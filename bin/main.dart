b1bd0f15d39263b43d5f40724b49c48990d80e68
kapil jhajhria <jhajhria44@gmail.com>
2019-10-15 09:29:46 +0530

    Complete

--- bin/main.dart
+++ bin/main.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 // Challenge 1
 // Create a function that returns the total number of steps it takes to transform each element to the maximal element in the list.
 // Each step consists of incrementing a digit by one.
@@ -7,7 +9,18 @@
 // incrementToTop([4, 3, 4]) ➞ 1
 // incrementToTop([3, 3, 3]) ➞ 0
 // incrementToTop([3, 10, 3]) ➞ 14
+int incrementToTop(List<int> numbersList) {
+  int count = 0;
+  int max = numbersList.reduce((curr, next) => curr > next ? curr : next);
 
+  for (int i = 0; i < numbersList.length; i++) {
+    for (int e = numbersList[i]; e < max; e++) {
+      count++;
+    }
+  }
+
+  return count;
+}
 // Challenge 2
 // In BlackJack, cards are counted with -1, 0, 1 values:
 //
@@ -23,5 +36,26 @@
 
 // Challenge 3
 // Write a test case each for previous two functions you wrote
+int blackjackNumber(var x) {
+  String y = x.toString();
+  if (y == '2' || y == '3' || y == '4' || y == '5' || y == '6') {
+    return 1;
+  } else if (y == '7' || y == '8' || y == '9') {
+    return 0;
+  } else {
+    return -1;
+  }
+}
 
-main() {}
+int count(List x) {
+  int count = 0;
+  for (int i = 0; i < x.length; i++) {
+    count = count + blackjackNumber(x[i]);
+  }
+  return count;
+}
+
+main() {
+  print(incrementToTop([3, 10, 3]));
+  print(count(["A", 5, 5, 2, 6, 2, 3, 8, 9, 7]));
+}
